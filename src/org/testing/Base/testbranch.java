85040e103b356efed37b761825351205caa9637a
AniketSingh9897 <yuviyuvi338@gmail.com>
2023-11-14 20:44:13 +0530

    test branch

--- src/org/testing/Base/testbranch.java
+++ src/org/testing/Base/testbranch.java
@@ -1,10 +1,11 @@
 package org.testing.Base;
 
-public class test123 {
+public class testbranch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("test git push");
+System.out.println("test branch");
 	}
 
 }
